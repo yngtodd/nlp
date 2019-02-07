05a37c26552970a51fc40910d31835b17fabed1c
yngtodd <young.todd.mk@gmail.com>
2019-02-07 10:38:01 -0500

    import

--- nlp/ml/metrics/metrics.py
+++ nlp/ml/metrics/metrics.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def accuracy_thresh(y_pred:Tensor, y_true:Tensor, thresh:float=0.5, sigmoid:bool=True):
     "Compute accuracy when `y_pred` and `y_true` are the same size."
     if sigmoid: y_pred = y_pred.sigmoid()
