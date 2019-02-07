fe9cc1fd37f01cc9b1e5c1a31570a9291bbbcce8
yngtodd <young.todd.mk@gmail.com>
2019-02-07 10:37:04 -0500

    Adding metrics

---
+++ nlp/ml/metrics/metrics.py
@@ -0,0 +1,4 @@
+def accuracy_thresh(y_pred:Tensor, y_true:Tensor, thresh:float=0.5, sigmoid:bool=True):
+    "Compute accuracy when `y_pred` and `y_true` are the same size."
+    if sigmoid: y_pred = y_pred.sigmoid()
+    return np.mean(((y_pred>thresh)==y_true.byte()).float().cpu().numpy(), axis=1).sum()
